9b6cb322d7c1f1b380e42fd9fa6cdc61e8cae18c
Ridote <daniridao@gmail.com>
2018-07-26 22:40:27 +0100

    Kinematic character

--- Entities/CollisionShape2D.gd
+++ Entities/CollisionShape2D.gd
@@ -1,12 +1,11 @@
-extends KinematicBody2D
+extends CollisionShape2D
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	
 	pass
 
 #func _process(delta):
