618de4932f5111cd5737d16b454a6d35321c047f
Ridote <daniridao@gmail.com>
2018-08-08 22:18:05 +0100

    bullets has different layers regarding the owner (enemy or ally)

--- Entities/Enemies/Turret.gd
+++ Entities/Enemies/Turret.gd
@@ -1,19 +1,20 @@
 extends StaticBody2D
+var bulletScene = preload("res://Entities/Skills and projectiles/Bullet.tscn")
+var bullet = null
+
 var awakeningDistance = 400
 var sleepingDistance = 600
 
 var player = null
 var awaken = false
+var reloading = false
+var cannonReady = true
+
 
 func _ready():
 	player = get_node("/root/Main/Player")
+	$Cannon.flip_h = true
 	sleep()
-func wakeUp():
-	$Sprite/AnimationPlayer.play("WakeUp")
-	yield( get_node("AnimationPlayer"), "finished" )
-func sleep():
-	$Sprite/AnimationPlayer.play("Slept")
-	yield( get_node("AnimationPlayer"), "finished" )
 	
 func _process(delta):
 	var distance2Hero = global_position.distance_to(player.global_position);
@@ -23,3 +24,43 @@ func _process(delta):
 	elif(distance2Hero > sleepingDistance):
 		sleep()
 		awaken = false
+	if(awaken):
+		$Cannon.look_at(player.global_position)
+		shoot()
+
+func wakeUp():
+	$Base/BaseAnimation.play("WakeUp")
+	yield($Base/BaseAnimation, "animation_finished" )
+	$CannonBase/CannonBaseAnimation.play("WakeUp")
+	yield($CannonBase/CannonBaseAnimation, "animation_finished" )
+	$Cannon/CannonAnimation.play("WakeUp")
+	yield($Cannon/CannonAnimation, "animation_finished")
+	awaken = true
+func sleep():
+	$Base/BaseAnimation.play("Slept")
+	$CannonBase/CannonBaseAnimation.play("Slept")
+	$Cannon/CannonAnimation.play("Slept")
+	awaken = false
+	
+func shoot():
+	if reloading:
+		reloading = false
+		$Reload.start()
+	elif cannonReady:
+		bullet = bulletScene.instance()
+		bullet.set_collision_layer_bit(16,1)
+		#Character and floors (1+4)
+		bullet.set_collision_mask(5)
+		bullet.position = global_position
+		bullet.fire_right()
+		bullet.velocity.x = -150
+		bullet.velocity.y = 50
+		
+		get_parent().add_child(bullet)
+		bullet = null
+		cannonReady = false
+		reloading = true
+			
+func _on_Reload_timeout():
+	reloading = false
+	cannonReady = true
--- Entities/Skills and projectiles/Bullet.gd
+++ Entities/Skills and projectiles/Bullet.gd
@@ -15,10 +15,11 @@ func fire_left():
 	material.set_shader_param("speed", abs(velocity.x));
 	$Fire.process_material = material;
 	$Fire.emitting = true;
-
+	
 func _physics_process(delta):
 	move_and_slide(velocity, Vector2(0, -1))
-	if(get_slide_collision(0)):
+	#We need to check whether there are collisions or not and then if there are take the first one cause we don't care about the rest, we will just be destroyed for now
+	if(get_slide_count() > 0 && get_slide_collision(0)):
 		queue_free()
 
 func _on_Lifetime_timeout():
