66ef369db61c83c7acf601d7f39954b01981139a
Ridote <daniridao@gmail.com>
2018-08-09 20:06:13 +0100

    Turret already shooting

--- Entities/Enemies/Turret.gd
+++ Entities/Enemies/Turret.gd
@@ -7,40 +7,45 @@ var sleepingDistance = 600
 
 var player = null
 var awaken = false
+var wakingUp = false
 var reloading = false
 var cannonReady = true
 
 
 func _ready():
 	player = get_node("/root/Main/Player")
+	awaken = false
 	$Cannon.flip_h = true
 	sleep()
 	
 func _process(delta):
 	var distance2Hero = global_position.distance_to(player.global_position);
-	if(distance2Hero < awakeningDistance && ! awaken):
+	if(distance2Hero < awakeningDistance && !awaken):
 		wakeUp()
-		awaken = true
 	elif(distance2Hero > sleepingDistance):
 		sleep()
-		awaken = false
-	if(awaken):
-		$Cannon.look_at(player.global_position)
+	elif(awaken):
 		shoot()
+	$Cannon.look_at(player.global_position)
 
 func wakeUp():
-	$Base/BaseAnimation.play("WakeUp")
-	yield($Base/BaseAnimation, "animation_finished" )
-	$CannonBase/CannonBaseAnimation.play("WakeUp")
-	yield($CannonBase/CannonBaseAnimation, "animation_finished" )
-	$Cannon/CannonAnimation.play("WakeUp")
-	yield($Cannon/CannonAnimation, "animation_finished")
-	awaken = true
+	if(!wakingUp):
+		wakingUp = true
+		awaken = false
+		$Base/BaseAnimation.play("WakeUp")
+		yield($Base/BaseAnimation, "animation_finished" )
+		$CannonBase/CannonBaseAnimation.play("WakeUp")
+		yield($CannonBase/CannonBaseAnimation, "animation_finished" )
+		$Cannon/CannonAnimation.play("WakeUp")
+		yield($Cannon/CannonAnimation, "animation_finished")
+		awaken = true
+		wakingUp = false
+	
 func sleep():
+	awaken = false
 	$Base/BaseAnimation.play("Slept")
 	$CannonBase/CannonBaseAnimation.play("Slept")
 	$Cannon/CannonAnimation.play("Slept")
-	awaken = false
 	
 func shoot():
 	if reloading:
@@ -51,10 +56,13 @@ func shoot():
 		bullet.set_collision_layer_bit(16,1)
 		#Character and floors (1+4)
 		bullet.set_collision_mask(5)
-		bullet.position = global_position
-		bullet.fire()
-		bullet.velocity.x = -150
-		bullet.velocity.y = 50
+		#We calculate the position regarding the cannon
+		bullet.position = Vector2($Cannon.global_position.x+32*cos($Cannon.rotation), $Cannon.global_position.y+32*sin($Cannon.rotation))
+		#We calculate the normalised speed regarding the cannon and the player position
+		bullet.velocity = Vector2(player.global_position.x - $Cannon.global_position.x, player.global_position.y - $Cannon.global_position.y).normalized()*900
+		#We need to set the rotation so the shader particles run correctly
+		bullet.rotation = $Cannon.rotation + PI
+		
 		
 		get_parent().add_child(bullet)
 		bullet = null
